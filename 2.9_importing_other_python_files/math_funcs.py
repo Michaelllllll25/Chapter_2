83ef1bc33917211d380988274ddf4039cbcc4d23
Michael <90289974+Michaelllllll25@users.noreply.github.com>
2021-10-06 09:56:46 -0400

    Create math_funcs.py

---
+++ 2.9_importing_other_python_files/math_funcs.py
@@ -0,0 +1,13 @@
+def add(a, b):
+    return a + b
+
+def mult(a, b):
+    return a * b
+
+def main():
+    pass                   # would then run the words inside main()
+
+print(f"The name of the file is: {__name__}")     # types __main__
+
+if __name__ == "__main__":
+    main()
